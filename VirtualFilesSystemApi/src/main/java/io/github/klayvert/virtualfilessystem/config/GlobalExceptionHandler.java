1729a74018e5ef9cdad59de28d6cc76fa7e0b579
Klayvert2003 <klayvert.ryan16@gmail.com>
2024-09-19 22:02:26 -0300

    [FEAT]: Add GlobalExceptionHandler

---
+++ VirtualFilesSystemApi/src/main/java/io/github/klayvert/virtualfilessystem/config/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package io.github.klayvert.virtualfilessystem.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import java.util.Objects;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Object> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity
+                .status(ex.getStatusCode())
+                .body(
+                    Map.of(
+                        "error",
+                            Objects.requireNonNull(ex.getReason()
+                        )
+                    )
+                );
+    }
+}
