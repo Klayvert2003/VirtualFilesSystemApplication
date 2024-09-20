1ef35c04e17693365cff12787e074dcf61b26088
Klayvert2003 <klayvert.ryan16@gmail.com>
2024-09-20 20:35:24 -0300

    [FEAT]: Implement update, delete and findById methods to unit tests

--- VirtualFilesSystemApi/src/test/java/io/github/klayvert/virtualfilessystem/service/DirectoryServiceTests.java
+++ VirtualFilesSystemApi/src/test/java/io/github/klayvert/virtualfilessystem/service/DirectoryServiceTests.java
@@ -1,6 +1,7 @@
 package io.github.klayvert.virtualfilessystem.service;
 
 import io.github.klayvert.virtualfilessystem.domain.entities.Directory;
+import io.github.klayvert.virtualfilessystem.domain.entities.File;
 import io.github.klayvert.virtualfilessystem.domain.repositories.DirectoryRepository;
 import io.github.klayvert.virtualfilessystem.rest.dtos.DirectoryDTO;
 import org.junit.jupiter.api.Test;
@@ -8,20 +9,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-// TODO: Adicionar teste de update(apenas com diretório pai e com diretório filho)
-// TODO: Adicionar teste de delete
-// TODO: Adicionar teste de findById
-// TODO: Adicionar teste para capturar exceção de findById
+
 @ExtendWith(MockitoExtension.class)
 public class DirectoryServiceTests {
     @Mock
@@ -70,7 +68,7 @@ public class DirectoryServiceTests {
     }
 
     @Test
-    public void testSave() {
+    public void testSaveWithNoChildDirectory() {
         Directory directory = new Directory(
             1L,
             "directory1",
@@ -89,5 +87,245 @@ public class DirectoryServiceTests {
         DirectoryDTO savedDirectory = this.directoryService.findById(directory.getDirectoryId());
 
         assertNotNull(savedDirectory);
+    }
+
+    @Test
+    public void testSaveWithChildDirectory() {
+        Directory rootDirectory = new Directory(
+                1L,
+                "directory1",
+                LocalDate.now(),
+                new Directory(),
+                List.of(),
+                List.of()
+        );
+
+        Directory parentDirectory = new Directory(
+                2L,
+                "directory2",
+                LocalDate.now(),
+                rootDirectory,
+                List.of(),
+                List.of()
+        );
+
+        when(this.directoryRepository.save(rootDirectory)).thenReturn(new Directory());
+        this.directoryService.save(DirectoryDTO.toDTO(rootDirectory));
+        when(this.directoryRepository.findById(rootDirectory.getDirectoryId())).thenReturn(Optional.of(rootDirectory));
+        DirectoryDTO savedRootDirectory = this.directoryService.findById(rootDirectory.getDirectoryId());
+
+        when(this.directoryRepository.save(parentDirectory)).thenReturn(new Directory());
+        this.directoryService.save(DirectoryDTO.toDTO(parentDirectory));
+        when(this.directoryRepository.findById(parentDirectory.getDirectoryId())).thenReturn(Optional.of(parentDirectory));
+        DirectoryDTO savedParentDirectory = this.directoryService.findById(parentDirectory.getDirectoryId());
+
+        assertNotNull(savedRootDirectory);
+        assertNotNull(savedParentDirectory);
+    }
+
+    @Test
+    public void testUpdateWithChildDirectory() {
+        List<File> files = List.of(
+            new File(
+                1L,
+                "file1",
+                LocalDate.now(),
+                new Directory()
+            )
+        );
+
+        Directory parentDirectory = new Directory(
+                2L,
+                "parentDirectory",
+                LocalDate.now(),
+                null,
+                List.of(),
+                List.of()
+        );
+
+        Directory directory = new Directory(
+                1L,
+                "directory1",
+                LocalDate.now(),
+                parentDirectory,
+                List.of(),
+                files
+        );
+
+        when(this.directoryRepository.findById(2L)).thenReturn(Optional.of(parentDirectory));
+        when(this.directoryRepository.save(directory)).thenReturn(directory);
+
+        DirectoryDTO savedDirectory = this.directoryService.save(DirectoryDTO.toDTO(directory));
+
+        when(this.directoryRepository.findById(savedDirectory.getDirectoryId())).thenReturn(Optional.of(directory));
+        savedDirectory = this.directoryService.findById(1L);
+
+        String updatedName = "directory1_updated";
+        List<File> updatedFiles = List.of(
+            new File(
+                2L,
+                "file2",
+                LocalDate.now(),
+                new Directory()
+            )
+        );
+
+        Directory updatedDirectory = new Directory(
+                savedDirectory.getDirectoryId(),
+                updatedName,
+                savedDirectory.getCreatedAt(),
+                savedDirectory.getParentDirectory(),
+                savedDirectory.getSubDirectories(),
+                updatedFiles
+        );
+
+        when(this.directoryRepository.save(updatedDirectory)).thenReturn(updatedDirectory);
+
+        DirectoryDTO updatedDirectoryDTO = this.directoryService.update(DirectoryDTO.toDTO(updatedDirectory));
+
+        when(this.directoryRepository.findById(updatedDirectoryDTO.getDirectoryId())).thenReturn(Optional.of(updatedDirectory));
+
+        DirectoryDTO findUpdatedDirectory = this.directoryService.findById(updatedDirectoryDTO.getDirectoryId());
+
+        assertNotNull(savedDirectory);
+        assertNotNull(findUpdatedDirectory);
+        assertNotEquals(savedDirectory.getDirectoryName(), findUpdatedDirectory.getDirectoryName());
+        assertNotEquals(savedDirectory.getFiles(), findUpdatedDirectory.getFiles());
+        assertNotEquals(savedDirectory.getFiles().getFirst().getId(), findUpdatedDirectory.getFiles().getFirst().getId());
+        assertNotEquals(savedDirectory.getFiles().getFirst().getFileName(), findUpdatedDirectory.getFiles().getFirst().getFileName());
+        assertNotEquals(savedDirectory.getFiles(), findUpdatedDirectory.getFiles());
+        assertEquals(savedDirectory.getFiles().size(), updatedDirectoryDTO.getFiles().size());
+        assertEquals(findUpdatedDirectory.getDirectoryName(), updatedName);
+        assertEquals(findUpdatedDirectory.getFiles(), updatedFiles);
+    }
+
+
+    @Test
+    public void testUpdateWithNoChildDirectory() {
+        List<File> files = List.of(
+            new File(
+                1L,
+                "file1",
+                LocalDate.now(),
+                new Directory()
+            )
+        );
+
+        Directory directory = new Directory(
+                1L,
+                "directory1",
+                LocalDate.now(),
+                new Directory(),
+                List.of(),
+                files
+        );
+
+        when(this.directoryRepository.save(directory)).thenReturn(directory);
+
+        DirectoryDTO savedDirectory = this.directoryService.save(DirectoryDTO.toDTO(directory));
+
+        when(this.directoryRepository.findById(savedDirectory.getDirectoryId())).thenReturn(Optional.of(directory));
+
+        savedDirectory = this.directoryService.findById(1L);
+
+        String updatedName = "directory1_updated";
+        List<File> updatedFiles = List.of(
+            new File(
+                2L,
+                "file2",
+                LocalDate.now(),
+                new Directory()
+            )
+        );
+
+        Directory updatedDirectory = new Directory(
+                savedDirectory.getDirectoryId(),
+                updatedName,
+                savedDirectory.getCreatedAt(),
+                savedDirectory.getParentDirectory(),
+                savedDirectory.getSubDirectories(),
+                updatedFiles
+        );
+
+        when(this.directoryRepository.save(updatedDirectory)).thenReturn(updatedDirectory);
+
+        DirectoryDTO updatedDirectoryDTO = this.directoryService.update(DirectoryDTO.toDTO(updatedDirectory));
+
+        when(this.directoryRepository.findById(updatedDirectoryDTO.getDirectoryId())).thenReturn(Optional.of(updatedDirectory));
+
+        DirectoryDTO findUpdatedDirectory = this.directoryService.findById(updatedDirectoryDTO.getDirectoryId());
+
+        assertNotNull(savedDirectory);
+        assertNotNull(findUpdatedDirectory);
+        assertNotEquals(savedDirectory.getDirectoryName(), findUpdatedDirectory.getDirectoryName());
+        assertNotEquals(savedDirectory.getFiles(), findUpdatedDirectory.getFiles());
+        assertNotEquals(savedDirectory.getFiles().getFirst().getId(), findUpdatedDirectory.getFiles().getFirst().getId());
+        assertNotEquals(savedDirectory.getFiles().getFirst().getFileName(), findUpdatedDirectory.getFiles().getFirst().getFileName());
+        assertNotEquals(savedDirectory.getFiles(), findUpdatedDirectory.getFiles());
+        assertEquals(savedDirectory.getFiles().size(), updatedDirectoryDTO.getFiles().size());
+        assertEquals(findUpdatedDirectory.getDirectoryName(), updatedName);
+        assertEquals(findUpdatedDirectory.getFiles(), updatedFiles);
+    }
+
+    @Test
+    public void testDelete() {
+        List<File> files = List.of(
+            new File(
+                1L,
+                "file1",
+                LocalDate.now(),
+                new Directory()
+            )
+        );
+
+        Directory directory = new Directory(
+                1L,
+                "directory1",
+                LocalDate.now(),
+                new Directory(),
+                List.of(),
+                files
+        );
+
+        when(this.directoryRepository.findById(directory.getDirectoryId())).thenReturn(Optional.of(directory));
+        this.directoryService.delete(directory.getDirectoryId());
+
+        verify(this.directoryRepository, times(1)).delete(directory);
+
+        when(this.directoryRepository.findById(directory.getDirectoryId())).thenReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> this.directoryService.findById(directory.getDirectoryId()));
+    }
+
+    @Test
+    public void testFindByID() {
+        List<File> files = List.of(
+            new File(
+                1L,
+                "file1",
+                LocalDate.now(),
+                new Directory()
+            )
+        );
+
+        Directory directory = new Directory(
+            1L,
+            "directory1",
+            LocalDate.now(),
+            new Directory(),
+            List.of(),
+            files
+        );
+
+        when(this.directoryRepository.findById(directory.getDirectoryId())).thenReturn(Optional.of(directory));
+        Directory findDirectory = DirectoryDTO.toOBJ(this.directoryService.findById(directory.getDirectoryId()));
+
+        assertNotNull(findDirectory);
+        assertEquals(directory, findDirectory);
+    }
+
+    @Test
+    public void testFindByIdNotFoundException() {
+        assertThrows(ResponseStatusException.class, () -> this.directoryService.findById(null));
     }
 }
