2acb37abc27164eeb491847f24424d2a63d334ac
Klayvert2003 <klayvert.ryan16@gmail.com>
2024-09-21 10:54:40 -0300

    [FEAT]: Add custom button and router link

--- virtual-files-system-front/src/app/features/list/list.component.ts
+++ virtual-files-system-front/src/app/features/list/list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { Directory } from '../../shared/interfaces/directory.interface';
 import { DirectoriesService } from '../../shared/services/directories.service';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
