4b6a09a6530c68d9b9886f0c96c934e1e479d61e
Klayvert2003 <klayvert.ryan16@gmail.com>
2024-09-21 10:19:43 -0300

    [FEAT]: Implement simple card

--- virtual-files-system-front/src/app/features/list/list.component.ts
+++ virtual-files-system-front/src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Directory } from '../../shared/interfaces/directory.interface';
 import { DirectoriesService } from '../../shared/services/directories.service';
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
