ee48038a62a6211f6978890d52a12a7b8a357153
Klayvert2003 <klayvert.ryan16@gmail.com>
2024-09-23 23:27:39 -0300

    [FEAT]: Implement matSnackBar to info user when the dir is empty

--- virtual-files-system-front/src/app/features/list/components/card-details/card-details.component.ts
+++ virtual-files-system-front/src/app/features/list/components/card-details/card-details.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, RouterModule, Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Directory } from '../../../../shared/interfaces/directory.interface';
 import { DirectoriesService } from '../../../../shared/services/directories.service';
 import { CardComponent } from "../card/card.component";
 import { CardFileComponent } from '../card-file/card-file.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-card-details',
   standalone: true,
-  imports: [RouterModule, CardComponent, CardFileComponent],
+  imports: [RouterModule, CardComponent, CardFileComponent, MatButtonModule],
   templateUrl: './card-details.component.html',
   styleUrl: './card-details.component.scss'
 })
 
 export class CardDetailsComponent {
+  snackBar = inject(MatSnackBar);
+
   directory: Directory | null = null;
 
-  constructor(private route: ActivatedRoute, private router: Router, private directoryService: DirectoriesService) {}
+  constructor(private route: ActivatedRoute, private directoryService: DirectoriesService, private location: Location) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -29,10 +34,22 @@ export class CardDetailsComponent {
     this.directoryService.findById(directoryId).subscribe({
       next: (data) => {
         this.directory = data;
+
+        if ((this.directory?.subDirectories ?? []).length === 0 && (this.directory?.files ?? []).length === 0) {
+          this.snackBar.open('Pasta vazia', 'Fechar', {
+            duration: 3000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        }
       },
       error: (err) => {
         console.error('Erro ao buscar diretório:', err);
       }
     });
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
