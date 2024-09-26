1b4098657374531c7c85c88336aceda483c4ba7b
Klayvert2003 <klayvert.ryan16@gmail.com>
2024-09-25 22:01:34 -0300

    [FEAT]: Add option icon button with delete and update options

--- virtual-files-system-front/src/app/features/list/components/card/card.component.ts
+++ virtual-files-system-front/src/app/features/list/components/card/card.component.ts
@@ -1,13 +1,15 @@
 import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatMenuModule } from '@angular/material/menu'
 import { Directory } from '../../../../shared/interfaces/directory.interface';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, RouterLink],
+  imports: [MatCardModule, MatButtonModule, RouterLink, MatIconModule, MatMenuModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
